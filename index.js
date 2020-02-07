3145626eb1bee373eb995a2c580d38a37f346114
Noah Preszler <npreszler@ximasoftware.com>
2020-02-07 13:59:27 -0700

    Saving state. Not working.

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
-let round = 0;
-let rollCount = 0;
-let numPlayers = 6;
-let scores = new Array(numPlayers).fill(0);
-
-let rollDice = () => {
-    return Math.floor(Math.random() * 6) + Math.floor(Math.random() * 6) + 2;
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
 };
 
-let preMarket = () => {
+const preMarket = () => {
     let out = 0;
 
     for(let i = 0; i<3; i++) {
-        let roll = rollDice();
+        const roll = rollDie() + rollDie();
         out += roll == 7 ? 70 : roll;
-        rollCount++;
     }
 
     return out;
 };
 
-let postMarket = () => {
-    
+const postMarketRoll = () => {
+    const die1 = rollDie();
+    const die2 = rollDie();
+
+    if(die1 == die2) {
+        return -1;
+    } else if(die1 + die2 == 7) {
+        return 0;
+    } else {
+        return die1 + die2;
+    }
 };
 
-console.log(rollDice());
+const strategy1 = () => {
+    let score = 0;
+
+    for(let i = 0; i<20; i++) {
+        let rollCount = 3;
+        let roundScore = preMarket();
+
+        console.log(`Score: ${score}, Market opens at: ${roundScore}`);
+
+        if(rollCount < 6) {
+            let roll = postMarketRoll();
+            if(roll == -1) {
+                roundScore *= 2;
+            } else if(roll == 0) {
+                continue;
+            } else {
+                rollCount++;
+                roundScore += roll;
+            }
+
+            console.log(`  RollCount: ${rollCount}, Roll: ${roll}, RoundScore: ${roundScore}`);
+
+        } else {
+            score += roundScore;
+            console.log();
+            continue;
+        }
+    }
+
+    return score;
+};
+
+for(let i=0; i<30; i++) {
+    console.log(postMarketRoll());
+}
